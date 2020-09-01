443f29999128ed46c6e4a4318a8025cf7520a665
Raul Prado <rauleduardoandresprado@gmail.com>
2020-08-31 22:43:25 -0600

    RS-27 Se corrigio el template basado en roles.

--- resources/views/verorden/index.blade.php
+++ resources/views/verorden/index.blade.php
@@ -1,119 +1,78 @@
 @extends('layouts.app')
 
 @section('content')
-@role('cocinero')
 <div class="row">
-	<div class="col-sm-12">
-		<h1 align="center" >Ordenes Pendientes</h1>
-		<div class="row">
-		@foreach($ordens as $orden)
-		<div class="card col-md-4 mx-2 my-2 @if($orden->estado == 'En Proceso') border-success @endif">
-			<div class="card-body">			
-				<div class="page-header">
-					<div class="float-left">
-					Orden No. {{ $orden->id }}
-					</div>
-					<div class="float-right">
-						<p class="text-right">
-						<span class="badge badge-pill badge-primary">
-						{{$orden->created_at->diffForHumans()}}
-						</span>
-						</p>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-				<h6 class="card-subtitle mb-2 text-muted">{{ $orden->nombre }} | {{ $orden->nit }}</h6>
-    			<p class="card-text">Estado: {{ $orden->estado }}</p>
-			</div>
-			<ul class="list-group list-group-flash">
-			</ul>
-			<div class="card-body">
-				<div class="row">
-					<div class="col-sm-4 px-1">
-						<form action="{{ route('verorden.update', $orden)}}" method="post">
-							@csrf
-							@method('PUT')
-							<input type="hidden" name="estado" value=2>
-							<button class="btn btn-primary" type="submit">En Proceso</button>
-						</form>
-					</div>
-					<div class="col-sm-4 px-1">
-						<form action="{{ route('verorden.update', $orden)}}" method="post">
-							@csrf
-							@method('PUT')
-							<input type="hidden" name="estado" value=3>
-							<button class="btn btn-primary" type="submit">Completado</button>
-						</form>
-					</div>
-					<div class="col-sm-4 px-1">
-						<form action="{{ route('verorden.update', $orden)}}" method="post">
-							@csrf
-							@method('PUT')
-							<input type="hidden" name="estado" value=5>
-							<button class="btn btn-danger" type="submit">Cancelar</button>
-						</form>
-					</div>				
-				</div>
-			</div>
-		  </div>
-		  @endforeach
-
-		 </div>
-	</div>
-</div>
-@endrole
- @role('mostrador')
- 	<div class="row">
-	<div class="col-sm-12">
+	<div class="col-sm-12">		
+		@role('mostrador')
 		<h1 align="center" >Ordenes Por Entregar </h1>
+		@else
+		<h1 align="center" >Ordenes Pendientes</h1>
+		@endrole
 		<div class="row">
-			@if ($ordens->count() == 0) 
-					<div href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-						<div class="d-flex w-100 justify-content-between">
-							<h5 class="mb-1">No hay ordenes :'(</h5>
+			@foreach($ordens as $orden)
+			<div class="card col-md-4 mx-2 my-2 @if($orden->estado == 'En Proceso') border-success @endif">
+				<div class="card-body">			
+					<div class="page-header">
+						<div class="float-left">
+							Orden No. {{ $orden->id }}
 						</div>
+						<div class="float-right">
+							<p class="text-right">
+								<span class="badge badge-pill badge-primary">
+									{{$orden->created_at->diffForHumans()}}
+								</span>
+							</p>
+						</div>
+						<div class="clearfix"></div>
 					</div>
-					@endif
-		@foreach($ordens as $orden)
-		<div class="card col-md-4 mx-2 my-2 @if($orden->estado == 'En Proceso') border-success @endif">
-			<div class="card-body">			
-				<div class="page-header">
-					<div class="float-left">
-					Orden No. {{ $orden->id }}
-					</div>
-					<div class="float-right">
-						<p class="text-right">
-						<span class="badge badge-pill badge-primary">
-						{{$orden->created_at->diffForHumans()}}
-						</span>
-						</p>
-					</div>
-					<div class="clearfix"></div>
+					<h6 class="card-subtitle mb-2 text-muted">{{ $orden->nombre }} | {{ $orden->nit }}</h6>
+					<p class="card-text">Estado: {{ $orden->estado }}</p>
 				</div>
-				<h6 class="card-subtitle mb-2 text-muted">{{ $orden->nombre }} | {{ $orden->nit }}</h6>
-    			<p class="card-text">Estado: {{ $orden->estado }}</p>
-			</div>
-			<ul class="list-group list-group-flash">
-			</ul>
-			<div class="card-body">
-				<div class="row">
-					
-					<div class="col-sm-4 px-1">
-						<form action="{{ route('verorden.update', $orden)}}" method="post">
-							@csrf
-							@method('PUT')
-							<input type="hidden" name="estado" value=4>
-							<button class="btn btn-primary" type="submit">Completado</button>
-						</form>
+				<ul class="list-group list-group-flash">
+				</ul>
+				<div class="card-body">
+					<div class="row">							
+						@role('mostrador')
+						<div class="col-sm-4 px-1">
+							<form action="{{ route('verorden.update', $orden)}}" method="post">
+								@csrf
+								@method('PUT')
+								<input type="hidden" name="estado" value=4>
+								<button class="btn btn-primary" type="submit">Completado</button>
+							</form>
+						</div>	
+						@else
+						<div class="col-sm-4 px-1">
+							<form action="{{ route('verorden.update', $orden)}}" method="post">
+								@csrf
+								@method('PUT')
+								<input type="hidden" name="estado" value=2>
+								<button class="btn btn-primary" type="submit">En Proceso</button>
+							</form>
+						</div>
+						<div class="col-sm-4 px-1">
+							<form action="{{ route('verorden.update', $orden)}}" method="post">
+								@csrf
+								@method('PUT')
+								<input type="hidden" name="estado" value=3>
+								<button class="btn btn-primary" type="submit">Completado</button>
+							</form>
+						</div>
+						<div class="col-sm-4 px-1">
+							<form action="{{ route('verorden.update', $orden)}}" method="post">
+								@csrf
+								@method('PUT')
+								<input type="hidden" name="estado" value=5>
+								<button class="btn btn-danger" type="submit">Cancelar</button>
+							</form>
+						</div>
+						@endrole	
 					</div>
-					
 				</div>
 			</div>
-		  </div>
-		  @endforeach
+			@endforeach
 
-		 </div>
+		</div>
 	</div>
 </div>
- @endrole
 @endsection
